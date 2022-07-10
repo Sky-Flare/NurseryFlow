057d0be2f3555deabd13300d679b0ea174e91b32
Jocelyn Lebaad <lebaad.jocelyn@gmail.com>
2022-07-10 10:53:36 +0200

    wip

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,56 +1,32 @@
 <template>
   <h1>Task</h1>
+  <div class="cursor-pointer" @click="launch">GO</div>
   <div>
     <form @submit.prevent="onSubmit" class="add-form">
       <div class="form-control">
         <label>Name</label>
-        <input
-          type="text"
-          v-model="name"
-          name="name"
-          placeholder="Enter Your Name"
-        />
+        <input type="text" v-model="name" name="name" placeholder="Enter Your Name" />
       </div>
       <input type="submit" value="Save Information" class="btn btn-block" />
     </form>
   </div>
-  <div class="flex">
-    <span
-      v-for="user in users"
-      :key="user.name"
-      @click="userToview = user.name"
-      class="mr-2 cursor-pointer"
-    >
-      {{ user.name }}+ {{ user.tasks.length }}</span
-    >
+  <div class="flex flex-wrap">
+    <span v-for="user in users" :key="user.name" @click="userToview = user.name" class="mr-2 cursor-pointer"
+      :class="user.tasks.length !== days ? 'text-red-400' : 'text-green-400'">
+      {{ user.name }}+ {{ user.tasks.length }}</span>
   </div>
-  <div
-    class="flex flex-col border-2 mb-2"
-    v-for="day in days"
-    :key="day"
-    :day="day"
-  >
+  <div class="flex flex-col border-2 mb-2" v-for="day in days" :key="day" :day="day">
     {{ day }}
     <div class="flex justify-between">
-      <div
-        class="flex flex-col"
-        v-for="(task, idxTask) in tasks"
-        :key="idxTask"
-        :task="task"
-      >
+      <div class="flex flex-col" v-for="(task, idxTask) in tasks" :key="idxTask" :task="task">
         <span class="font-bold">{{ task.name }}</span>
         <div class="flex flex-col">
-          <div
-            v-for="(user, idxUser) in getUsers(day, task)"
-            :key="idxUser"
-            @click="userToview = user.name"
-            class="cursor-pointer"
-            :class="
+          <div v-for="(user, idxUser) in getUsers(day, task)" :key="idxUser" @click="userToview = user.name"
+            class="cursor-pointer" :class="
               userToview === user.name
                 ? 'opacity-100	bg-orange-500	'
                 : 'opacity-80'
-            "
-          >
+            ">
             {{ user.name }}
           </div>
         </div>
@@ -73,34 +49,33 @@ interface user {
 const tasks = [
   {
     name: "sandwichs",
-    nbUser: 3,
+    nbUser: 4,
   },
   {
     name: "petit dej",
-    nbUser: 3,
+    nbUser: 4,
   },
   {
     name: "vaisselle pti dej",
-    nbUser: 3,
+    nbUser: 4,
   },
   {
     name: "table",
-    nbUser: 3,
+    nbUser: 4,
   },
-  /*{
+  {
     name: "repas",
-    nbUser: 5,
+    nbUser: 4,
   },
   {
     name: "vaisselle soir",
-    nbUser: 4,
-  }, */
+    nbUser: 5,
+  },
 ];
 const userToview = ref();
 const days = ref(6);
 let users = ref<user[]>([]);
 const name = ref("");
-onMounted(() => {});
 
 function onSubmit() {
   console.log(name.value);
@@ -115,49 +90,41 @@ const persPerTask = computed(() => {
   return Math.round(users.value.length / tasks.length);
 });
 function launch() {
+
   setUser();
-  let securit = 0;
   if (checkUsersToTasks()) {
     //Pour chaque jours
 
     for (let day = 0; day <= days.value - 1; day++) {
-      console.log(
-        "JOURS __________________________________________________",
-        day
-      );
-             let array: user[] = fisherYatesShuffle(users.value).slice();
-       
-/*       let array: user[] = users.value;
- */
-      //Pour chaque taches
+      let array: user[] = fisherYatesShuffle(users.value);
+
+      //Pour chaque taches 
       tasks.forEach((task) => {
-        console.log("      TACHES ", task.name);
 
         let nbUsersTask = 0;
 
         array.forEach((user) => {
-          const lastTask = user.tasks.find((t) => t.day == day - 1);
-
+          const lastTask = user.tasks.find((t) => t.day === day - 1);
           if (
             (nbUsersTask < task.nbUser &&
               lastTask?.name !== task.name &&
-              countSameTask(user, task.name) < 2) ||
+              countSameTask(user, task.name) < Math.ceil(((tasks.length * days.value) / users.value.length))) ||
             (nbUsersTask < task.nbUser &&
               array[array.length - 1].name === user.name)
           ) {
-            const found = user.tasks.find((element) => element.day === day);
-            console.log("found", found);
+            const foundTask = user.tasks.find((element) => element.day === day);
+            // console.log("found", foundTask); 
 
-            if (found === undefined) {
-              console.log("in if");
+            if (foundTask === undefined) {
+              // console.log("in if");
 
               const taskCreat = { name: task.name, day: day };
-              console.log("taskCreat", taskCreat);
+              // console.log("taskCreat", taskCreat);
 
               user.tasks.push(taskCreat);
               nbUsersTask++;
             } else {
-              console.log("not found ", user);
+              // console.log("not found ", user);
             }
           }
         });
@@ -174,9 +141,9 @@ function checkUsersToTasks() {
   if (nbUserTotal !== users.value.length) {
     alert(
       "Nombre de user par tache pas identique au nombre de user total. Nb users : " +
-        users.value.length +
-        "Nb users total taches " +
-        nbUserTotal
+      users.value.length +
+      "Nb users total taches " +
+      nbUserTotal
     );
   }
   return nbUserTotal === users.value.length;
@@ -203,7 +170,7 @@ function countSameTask(user: user, taskSearch: string) {
 }
 /* const x = (task.nbUser * 100) / users.value.length;
 console.log("x", task.name, Math.ceil((days.value / 100) * x)); */
-/* countSameTask(user, task.name) <=
+/* countSamecalTask(user, task.name) <=
               Math.ceil((days.value / 100) * x) && */
 /*    user.tasks.length < day + 1 && */
 /*    (user.tasks || user.tasks[day - 1].name !== task.name) && */
@@ -222,8 +189,6 @@ Vaisselle du soir
 */
 
 function getUsers(day: number, task: { name: string; nbUser: number }) {
-  console.log("getUsers DAY", day);
-  console.log("USer", users.value);
 
   return users.value.filter((user) => {
     return user.tasks.find(
@@ -279,11 +244,7 @@ function setUser() {
     name: "porcinet",
     tasks: [],
   });
-  users.value.push({
-    name: "timon",
-    tasks: [],
-  });
-  /* users.value.push({ name: "martin", tasks: [] });
+  users.value.push({ name: "martin", tasks: [] });
   users.value.push({ name: "loulou", tasks: [] });
   users.value.push({ name: "mastu", tasks: [] });
   users.value.push({ name: "amixem", tasks: [] });
@@ -297,7 +258,7 @@ function setUser() {
   users.value.push({ name: "mathilde", tasks: [] });
   users.value.push({ name: "tim", tasks: [] });
   users.value.push({ name: "tom", tasks: [] });
-  users.value.push({ name: "tchou", tasks: [] });
+  /*users.value.push({ name: "tchou", tasks: [] });
   users.value.push({ name: "lili", tasks: [] });
   users.value.push({ name: "max", tasks: [] });
   users.value.push({ name: "toms", tasks: [] });
