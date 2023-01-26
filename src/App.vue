d2aaadce95f9ab623ff80a4dbdcaddc0d8c5813b
Jocelyn Lebaad <lebaad.jocelyn@gmail.com>
2023-01-26 22:39:12 +0100

    test scroll

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,68 @@
 <template>
-  <Tasks />
+  <div ref="container">
+    <template v-for="index in 200" :key="index">
+      <div
+        v-if="index > currentId - 5 && index < currentId + 5"
+        :ref="(el) => (elements[index] = el)"
+        :data-id="index"
+      >
+        <Tasks :index="index" :key="index" />
+      </div>
+    </template>
+  </div>
 </template>
 
 <script setup lang="ts">
+import {
+  ComponentPublicInstance,
+  onMounted,
+  ref,
+  watch,
+} from "@vue/runtime-dom";
 import Tasks from "./components/Tasks.vue";
+
+const elements = ref<Element[] | ComponentPublicInstance[] | null[]>([]);
+const container = ref<Element>();
+const observer = ref<IntersectionObserver>();
+const currentId = ref(1);
+
+observer.value = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0.5) {
+        currentId.value = Number(entry.target.dataset.id);
+        console.log(currentId.value);
+      }
+    });
+  },
+  { threshold: [0.5] }
+);
+watch(
+  () => currentId.value,
+  () => {
+    console.log(currentId.value);
+    observer.value?.unobserve;
+    if (elements.value.length) {
+      elements.value.forEach((el) => {
+        const value = el as Element;
+        if (value) {
+          observer.value?.observe(value);
+        }
+      });
+    }
+  }
+);
+
+onMounted(() => {
+  if (elements.value.length) {
+    elements.value.forEach((el) => {
+      const value = el as Element;
+      if (value) {
+        observer.value?.observe(value);
+      }
+    });
+  }
+});
 </script>
 
 <style>
